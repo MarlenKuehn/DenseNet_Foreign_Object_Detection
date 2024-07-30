95dab2ab46210978df5fd9985246d0cf24922ea1
marlen <marlenkuehn97@gmail.com>
2024-07-30 18:00:30 +0200

    classify folder added

--- DenseNet/model/Usecases/classify_folder.py
+++ DenseNet/model/Usecases/classify_folder.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 import torchvision
 from torchvision import models, transforms
 from PIL import Image
-from collections import OrderedDict
 import numpy as np
+import os
 
 path_to_model = 'DenseNet/model/Trained_Models/Training_C.pth'
-path_to_image = 'DenseNet/Images/0/47784381729225956156725553701446715500_sgkre8.png'
+path_to_folder = 'DenseNet/Images/1/'
 
 chosen_model = "DenseNet201"  # "DenseNet201" or "DenseNet169"
 
@@ -18,6 +18,7 @@ if chosen_model == "DenseNet169":
 elif chosen_model == "DenseNet201":
     net = models.densenet201(weights=models.DenseNet201_Weights.IMAGENET1K_V1)
     net.classifier = nn.Linear(net.classifier.in_features, 1)
+
 
 def load_model(path_to_model, net):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -30,39 +31,44 @@ def load_model(path_to_model, net):
 # Load the model
 net = load_model(path_to_model, net)
 
-# Define the transformation
 testTransform = transforms.Compose([
         torchvision.transforms.Grayscale(num_output_channels=3),
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
     ])
 
+# Optional: display the image
 def show_image(image_tensor):
     # Convert the tensor to a PIL image
     unloader = transforms.ToPILImage()
     image = unloader(image_tensor)
     image.show()
-
-# Try to open the image and normalize it 
-try:   
+    
+def process_image(path_to_image):
     arr = np.array(Image.open(path_to_image))
     arr = (arr - arr.min()) * 255 // (arr.max() - arr.min() + 1e-10)
     image = Image.fromarray(arr.astype("uint8"))
-except Exception as e:
-    print("Error while opening image: ", e)
+    image = testTransform(image).unsqueeze(0)
+    return image
 
-# Transform the image
-image = testTransform(image).unsqueeze(0)
+def predict_image(net, image):
+    with torch.no_grad():
+        output = net(image)
+        probs = torch.sigmoid(output)
+        preds = (probs > 0.5).int()  
+        class_names = ['NO', 'YES']  # NO: 'There is no electrical device', YES: 'There is an electrical device'
+        return class_names[int(preds.item())]
 
-# Optional: display the image
-#show_image(image)
-
-# Predict the image class
-with torch.no_grad():
-    output = net(image)
-    probs = torch.sigmoid(output)
-    preds = (probs > 0.5).int()  
-    class_names = ['NO', 'YES']  # NO: 'There is no electrical device', YES: 'There is an electrical device'
-    print("Is there a foreign object in the image? ",class_names[int(preds.item())])
-
-
+for image_file in os.listdir(path_to_folder):
+    image_path = os.path.join(path_to_folder, image_file)
+    # Check if the file is an image
+    if image_path.endswith(('.png', '.jpg', '.jpeg')): 
+        # Try to open the image
+        try:
+            # Normalize and transform the image
+            image_tensor = process_image(image_path)
+            # Predict the image class
+            prediction = predict_image(net, image_tensor)
+            print(f"Is there a foreign object in the image {image_file}? {prediction}")
+        except Exception as e:
+            print(f"Error processing image {image_file}: {e}")
--- DenseNet/model/Usecases/test_image_transformation.py
+++ DenseNet/model/Usecases/test_image_transformation.py
@@ -1,22 +1,15 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from PIL import Image
-from skimage import io
 import numpy as np
 import torchvision.transforms as transforms
 import torchvision
-import torch
 
-# Load an image
+# Load an image and normailze it
 arr = np.array(Image.open("DenseNet/Images/0/7229642368407186565500544115495475569_ja0kjg.png"))
 arr = (arr - arr.min()) * 255 // (arr.max() - arr.min() + 1e-10)
 img = Image.fromarray(arr.astype("uint8"))
+
+# Show the original image
 img.show()
-
-
-normMean = [0.49139968]
-normStd = [0.24703233]
-normTransform = transforms.Normalize(normMean, normStd)
 
 trainTransform = transforms.Compose([
     torchvision.transforms.Grayscale(num_output_channels=3),
@@ -26,9 +19,9 @@ trainTransform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.RandomAdjustSharpness(2, p=0.5),
     transforms.ToTensor(),
-    #normTransform
 ])
 
+# Transform the image
 transformed_image = trainTransform(img)
 
 def show_image(image_tensor):
@@ -36,37 +29,6 @@ def show_image(image_tensor):
     unloader = transforms.ToPILImage()
     image = unloader(image_tensor)
     image.show()
-
+    
+# Display the transformed image
 show_image(transformed_image)
-
-def denormalize(tensor, mean, std):
-    mean = torch.tensor(mean)
-    std = torch.tensor(std)
-    for t, m, s in zip(tensor, mean, std):
-        t.mul_(s).add_(m)
-    return tensor
-
-
-# Denormalize the transformed image tensor
-denorm_image_tensor = denormalize(transformed_image.clone(), [0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-
-# Convert the tensor to a numpy array for visualization
-transformed_image_array = denorm_image_tensor.numpy().transpose((1, 2, 0))
-
-# Clip the values to be in the valid range [0, 1]
-transformed_image_array = np.clip(transformed_image_array, 0, 1)
-
-# Display the original and transformed images
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-
-# Original image
-axs[0].imshow(img, cmap='gray')
-axs[0].set_title("Original Image")
-axs[0].axis('off')
-
-# Transformed image
-axs[1].imshow(transformed_image_array)
-axs[1].set_title("Transformed Image")
-axs[1].axis('off')
-
-#plt.show()
